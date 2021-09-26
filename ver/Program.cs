75f5d0cd317501b65156b4eb867b9f69f1a0270e
aerMoon <silfochka@gmail.com>
2021-09-26 18:02:26 +0500

    Добавьте файлы проекта.

---
+++ ver/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 20;
+            for (i=20; i>=0; i-=2)
+            {
+                Console.WriteLine(i);
+
+            }
+        }
+    }
+}
